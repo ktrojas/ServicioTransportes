db02e63204b488ac2a89c3f0cabaf2726bf97145
Kt Rojas <ktandrearojas@gmail.com>
2022-04-26 08:13:31 -0500

    init

---
+++ application.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask, jsonify, request
+from config import config
+from flaskext.mysql import MySQL
+
+application = Flask(__name__)
+application.config.from_object(config['development'])
+
+conexion = MySQL(application)
+
+@application.route('/containers/<json_obj>', methods=['POST'])
+def hellow_world(json_obj):
+    try:
+        jsonn = request.get_json(json_obj)
+        #print(request.json['listaContenedores'])
+        print(jsonn["presupuesto"])
+        listaContenedores = jsonn['listaContenedores']
+        print("--------------------------------------------")
+        for data in listaContenedores:
+            print("Nombre: ", data["nombreContenedor"])
+            print("Costo: ", data["costo"])
+            print("KPI: ", data["kpi"])
+        return jsonify({'Mensaje': 'Contenedor Registrado'})
+    except Exception as e:
+        return jsonify({'Mensaje': 'Error'})
